2b667a78a608dc893b05589c4d60110f1bf702d7
Debian <joseph.garrone.gj@gmail.com>
2018-01-22 09:07:49 +0000

    add missing files

---
+++ src/lib/web.ts
@@ -0,0 +1,101 @@
+import * as https from "https";
+import * as http from "http";
+import * as express from "express";
+import * as session from "express-session";
+import * as forceDomain from "forcedomain";
+
+import * as networkTools from "../tools/networkTools";
+import * as webApiServer from "./webApiServer";
+import { handlers as apiHandlers } from "./webApiServerImplementation";
+import * as webPagesServer from "./webPagesServer";
+import * as sipProxy from "./sipProxy";
+
+import * as WebSocket from "ws";
+
+import { c } from "./_constants";
+
+
+import * as _debug from "debug";
+let debug = _debug("_web");
+
+export interface Session extends Express.Session {
+    auth?: Session.Auth;
+};
+
+export namespace Session {
+
+    export type Auth= {
+        user: number;
+        email: string;
+    };
+}
+
+export async function start() {
+
+    let hostname = `www.${c.shared.domain}`;
+
+    let { interfaceIp } = await networkTools.retrieveIpFromHostname(hostname);
+
+    await new Promise<void>(
+        resolve => {
+
+            let app = express();
+
+            app
+                .use(forceDomain({ hostname }))
+                .use(session({ "secret": "xSoLe9d3=", "resave": false, "saveUninitialized": false }))
+                ;
+
+            webApiServer.start(app, apiHandlers);
+
+            webPagesServer.start(app);
+
+            let httpsServer = https.createServer(c.tlsOptions);
+
+            let webSocketServer = new WebSocket.Server({ "server": httpsServer });
+
+            httpsServer
+                .on("request", app)
+                .listen(443, interfaceIp)
+                .once("listening", () => resolve())
+                ;
+
+            webSocketServer.on("connection", (webSocket, req) =>
+                sipProxy.onClientConnection(
+                    webSocket,
+                    {
+                        "localPort": 443,
+                        "remotePort": req.socket.remotePort,
+                        "localAddress": interfaceIp,
+                        "remoteAddress": req.socket.remoteAddress
+                    }
+                )
+            );
+
+        }
+    );
+
+    debug(`...webserver started`);
+
+    await new Promise<void>(
+        resolve => {
+
+            let app = express();
+
+            app.use(forceDomain({
+                hostname,
+                "port": 443,
+                "protocol": "https"
+            }));
+
+            http.createServer()
+                .on("request", app)
+                .listen(80, interfaceIp)
+                .once("listening", () => resolve());
+
+        }
+    );
+
+    debug(`...http redirect to https started`);
+
+}
---
+++ src/lib/webApiServer.ts
@@ -0,0 +1,146 @@
+import * as express from "express";
+import { Session } from "./web";
+import { webApiDeclaration } from "../semasim-frontend";
+const apiPath = webApiDeclaration.apiPath;
+import JSON_CUSTOM= webApiDeclaration.JSON_CUSTOM;
+
+import "colors";
+
+import * as _debug from "debug";
+let debug = _debug("_webApiServer");
+
+export type Handler<Params, Response>={
+    needAuth: boolean;
+    contentType: "application/json" | "application/xml";
+    sanityChecks?: (params: Params) => boolean;
+    handler: ( params: Params, session: Session, remoteAddress: string) => Promise<Response>;
+}
+
+export type Handlers = {
+    [methodName: string]: Handler<any, any>;
+};
+
+const httpCodes = {
+    "ok": 200,
+    "badRequest": 400,
+    "unauthorized": 401,
+    "internalServerError": 500,
+};
+
+async function bodyParser(req: express.Request): Promise<any> {
+
+    let rawBody = "";
+
+    let timer = setTimeout(() => {
+        rawBody = "!";
+        req.emit("end");
+    }, 1000);
+
+    req.on("data", (buff: Buffer) => rawBody += buff.toString("utf8"));
+
+    await new Promise<void>(resolve => {
+        clearTimeout(timer);
+        req.once("end", () => resolve());
+    });
+
+    try {
+
+        return JSON_CUSTOM.parse(rawBody);
+
+    } catch{
+
+        throw new Error("Bad request");
+
+    }
+
+}
+
+export function start(
+    app: express.Express,
+    handlers: Handlers
+) {
+
+    let router = express.Router();
+
+    app.use(`/${apiPath}`, router);
+
+    router
+        .use("/:methodName", async (req, res) => {
+
+            let badRequest = () => {
+                (req.session! as Session).auth = undefined;
+                res.status(httpCodes.badRequest).end();
+            };
+
+            let { methodName } = req.params;
+
+            if (!handlers[methodName]) {
+                badRequest();
+                return;
+            }
+
+            let { handler, sanityChecks, needAuth, contentType } = handlers[methodName];
+
+            let session: Session = req.session!;
+
+            if (needAuth && !session.auth) {
+                res.status(httpCodes.unauthorized).end();
+                return;
+            }
+
+            let params: any;
+
+            switch (req.method) {
+                case "GET": params = req.query; break;
+                case "POST": 
+                    try{
+                        params= await bodyParser(req);
+                    }catch{
+                        badRequest();
+                        return;
+                    }
+                    break;
+                default: badRequest(); return;
+            }
+
+            if (sanityChecks && !sanityChecks(params)) {
+                badRequest();
+                return;
+            }
+
+            let response: any;
+
+            try {
+
+                response = await handler(
+                    params,
+                    session,
+                    req.connection.remoteAddress
+                );
+
+            } catch (error) {
+                res.status(httpCodes.internalServerError).end();
+                return;
+            }
+
+            res.setHeader("Content-Type", `${contentType}; charset=utf-8`)
+
+            res.status(httpCodes.ok);
+
+            let rawResponse: string;
+
+            if (contentType === "application/json") {
+
+                rawResponse = JSON_CUSTOM.stringify(response);
+
+            } else {
+
+                rawResponse = response;
+
+            }
+
+            res.send(new Buffer(rawResponse, "utf8"));
+
+        });
+
+}
